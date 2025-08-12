bc77c564fe2e4b93e96653e5bfd6109ec2e6449e
Tejodhay Vasantha <128919811+Tejodhay@users.noreply.github.com>
2025-08-12 18:50:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
 
